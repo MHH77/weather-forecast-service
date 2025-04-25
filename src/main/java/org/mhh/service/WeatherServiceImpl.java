2a758070c7f8fbb1f509af3049d3d28e1d6956e8
MHH77 <mesfandiari77@gmail.com>
2025-04-25 22:02:30 +0330

    [feature] implement service class

--- src/main/java/org/mhh/service/WeatherServiceImpl.java
+++ src/main/java/org/mhh/service/WeatherServiceImpl.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.mhh.dto.WeatherResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -16,6 +18,8 @@ public class WeatherServiceImpl implements WeatherService {
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
+
+    @Value("${openweathermap.api.key}")
     private String apiKey;
 
     @Value("${openweathermap.api.url}")
